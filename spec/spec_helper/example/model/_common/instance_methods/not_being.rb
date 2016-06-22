37e811c9431456bcd75fe54bb007195c6d53dfd7
indeep-xyz <indeep.xyz@gmail.com>
2016-06-22 20:22:32 +0900

    Fix a method in test
    - Change #send to #__send__
      - ::BasicObject, which is the superclass of PseudoObject::BasicObject, does not have #send

--- spec/spec_helper/example/model/_common/instance_methods/not_being.rb
+++ spec/spec_helper/example/model/_common/instance_methods/not_being.rb
@@ -16,7 +16,7 @@ RSpec.shared_examples 'instance methods not being' do
         }
 
         it 'should raise NoMethodError in @pseudo_object' do
-          expect { subject.send(method_name) }.to \
+          expect { subject.__send__(method_name) }.to \
               raise_error(::NoMethodError, regexp)
         end
       end
