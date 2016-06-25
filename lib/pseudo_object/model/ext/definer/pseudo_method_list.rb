08b6bf3728471db3d0eca485fe664c1dd6017ef9
indeep-xyz <indeep.xyz@gmail.com>
2016-06-25 15:29:43 +0900

    Change specification of a class method
    PseudoObject::BasicObject
    ----
    
    - Change specification of a class method
      - .pseudized_instance_methods
    - Remove class methods
      - .pseudized_methods
      - .private_pseudized_instance_methods
      - .protected_pseudized_instance_methods
      - .public_pseudized_instance_methods
    - Fix test

--- lib/pseudo_object/model/ext/definer/pseudo_method_list.rb
+++ lib/pseudo_object/model/ext/definer/pseudo_method_list.rb
@@ -13,7 +13,10 @@ module PseudoObject
 
         class << self
           def define_methods(cls)
-            new(cls).define_methods
+            instance = new(cls)
+
+            instance.define_methods
+            instance.define_pseudized_methods
           end
         end
 
@@ -23,9 +26,12 @@ module PseudoObject
 
         def define_methods
           METHOD_TYPES.each do |type|
-            define_pseudized_method(type)
             define_pseudo_method(type)
           end
+        end
+
+        def define_pseudized_methods
+          define_pseudized_method('instance_methods')
         end
 
         private
@@ -36,14 +42,18 @@ module PseudoObject
         # Define the class method of "pseudized_*" series
         #
         # It returns an array including the names of methods
-        # defined in PseudoObject and in the model.
+        # overriding the model's.
         #
         # @m [String] method name of the method to list methods
         def define_pseudized_method(m)
           @class.class_eval <<-EOT
             define_method('#{pseudized_method_name(m)}') do
-              #{pseudo_method_name(m)} \
-                  | pseudo_class.#{m}
+              collection = ancestors[0..-2].inject([]) do |result, cls|
+                result |= cls.#{m}
+              end & pseudo_class.#{m}
+
+              head_of_commons = collection.index(:instance_eval)
+              collection[0...head_of_commons]
             end
           EOT
         end
--- spec/spec_helper/example/model/basic_object/class_methods/valid_list_of_pseudo_methods.rb
+++ spec/spec_helper/example/model/basic_object/class_methods/valid_list_of_pseudo_methods.rb
@@ -2,28 +2,14 @@ RSpec.shared_examples 'valid list of pseudo methods' do
   # - - - - - - - - - - - - - - - - - - - -
   # .pseudized_* series
 
-  %w/
-  instance_methods
-  methods
-  private_instance_methods
-  protected_instance_methods
-  public_instance_methods
-  /.each do |method_name|
-    pseudized_method_name = "pseudized_#{method_name}"
-    pseudo_method_name = "pseudo_#{method_name}"
-    it_text = 'should return an array including %s and the model class\'s' % [
-        pseudo_method_name.gsub(/_/, ' ')]
+  describe '.pseudized_instance_methods' do
+    subject { described_class.pseudized_instance_methods }
 
-    describe ".#{pseudized_method_name}" do
-      subject { described_class.__send__(pseudized_method_name) }
-
-    it it_text do
-      original = described_class.pseudo_class.__send__(method_name)
-      addition = described_class.__send__(pseudo_method_name)
-
+    it 'should return an array including instance methods' do
       is_expected.to \
-          match_array(original | addition)
-      end
+          include(:==, :!=, :!)
+      is_expected.not_to \
+          include(:instance_eval, :__send__, :__id__)
     end
   end
 
